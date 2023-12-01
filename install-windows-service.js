2368edf418a87b667cf078f167e165a2b4682658
Mike <mike.bui@ewoosoft.com>
2023-12-01 09:03:51 +0700

    add window services

---
+++ install-windows-service.js
@@ -0,0 +1,29 @@
+let Service = require("node-windows").Service;
+
+const path = require("path");
+const scriptPath = path.join(__dirname, "/app.js");
+
+// Create a new service object
+let svc = new Service({
+  name: "Node application as Windows Service test",
+  description: "Node application as Windows Service",
+  // script: 'C:\\Users\\EVN0031\\Desktop\\Ewoosoft\\repositories\\vtsw\\node-app-ws\\app.js'
+  script: scriptPath,
+  mike: "123123123",
+  env: [
+    // {
+    //   name: "HOME",
+    //   value: process.env["USERPROFILE"], // service is now able to access the user who created its' home directory
+    // },
+    // {
+    //   name: "TEMP",
+    //   value: path.join(__dirname, "/temp"), // use a temp directory in user's home directory
+    // },
+  ],
+});
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on("install", function () {
+  svc.start();
+});
+svc.install();
---
+++ uninstall-windows.service.js
@@ -0,0 +1,18 @@
+var Service = require('node-windows').Service;
+// Create a new service object
+
+const path = require('path');
+const scriptPath = path.join(__dirname, '/app.js');
+
+var svc = new Service({
+  name:'Node application as Windows Service test',
+  description: 'Node application as Windows Service',
+  script: scriptPath
+});
+// Listen for the "uninstall" event so we know when it's done.
+svc.on('uninstall',function(){
+  console.log('Uninstall complete.');
+  console.log('The service exists: ', svc.exists);
+});
+// Uninstall the service.
+svc.uninstall();
